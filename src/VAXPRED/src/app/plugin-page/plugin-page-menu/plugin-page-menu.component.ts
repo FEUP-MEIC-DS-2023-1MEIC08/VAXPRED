4927cc6b9c841e0ff7c9e65716b53eaee5a7d0c6
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2023-10-19 16:37:34 +0100

    plugin-page-menu as component

---
+++ src/VAXPRED/src/app/plugin-page/plugin-page-menu/plugin-page-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Plugin } from '../../plugin';
+
+@Component({
+  selector: 'app-plugin-page-menu',
+  templateUrl: './plugin-page-menu.component.html',
+  styleUrls: ['./plugin-page-menu.component.css']
+})
+export class PluginPageMenuComponent {
+  @Input() selectedTab: number = 0;
+  @Input() plugin: Plugin = new Plugin(0, '', '');
+}
