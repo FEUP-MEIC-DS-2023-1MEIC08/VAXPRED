a4c1f093b676768c9182b7d4aac66c68d281f743
Fernando-A-Rocha <frocha2001@gmail.com>
2023-10-21 17:19:07 +0100

    add: user profile base (using CoreUI and Angular Material)
    Work in progress
    
    Co-Authored-By: Rodrigo Figueiredo <87013677+RodrigoNTC@users.noreply.github.com>

--- src/VAXPRED/src/app/app.module.ts
+++ src/VAXPRED/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,11 +9,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SideMenuComponent } from './side-menu/side-menu.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent,
     WorkspaceComponent,
     SideMenuComponent,
   ],
--- src/VAXPRED/src/app/user-profile/user-profile.component.ts
+++ src/VAXPRED/src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+import { GridModule } from '@coreui/angular';
+import { ButtonModule } from '@coreui/angular';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
+  standalone: true,
+  imports: [
+    MatCardModule, MatButtonModule,
+    GridModule, ButtonModule
+  ],
 })
 export class UserProfileComponent {
 
+  constructor() { }
 }
