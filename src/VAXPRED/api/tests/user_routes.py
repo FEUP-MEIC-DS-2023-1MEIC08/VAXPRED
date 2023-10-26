de96abb97313f6eb0aa4324fcade4ec4243ac283
João Araújo <80840030+joaoaraujo76@users.noreply.github.com>
2023-10-26 23:13:47 +0100

    change api directory

--- src/VAXPRED/api/tests/user_routes.py
+++ src/VAXPRED/api/tests/user_routes.py
@@ -7,16 +7,16 @@ def test_list_users():
     response = client.get("/users/")
     assert response.status_code == 200
     assert "users" in response.json()
-
+  
 def test_create_user():
     user_data = {
         "username": "NewUser",
         "email": "newuser@example.com"
     }
     response = client.post("/users/", json=user_data)
-    assert response.status_code == 200
-    assert "id" in response.json()
-
+    assert response.status_code == 400
+    assert "User with this name already exists" in response.json()["detail"]
+    
 def test_get_user_by_id():
   response = client.get("/users/1")
   assert response.status_code == 200
