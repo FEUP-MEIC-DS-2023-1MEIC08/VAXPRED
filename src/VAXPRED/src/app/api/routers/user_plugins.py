27a269c8c2d73e9a2941b85eea84fac00b83ad6f
Andre Soares <andre.soarestc13@gmail.com>
2023-10-26 14:59:05 +0100

    small fix

--- src/VAXPRED/src/app/api/routers/user_plugins.py
+++ src/VAXPRED/src/app/api/routers/user_plugins.py
@@ -2,16 +2,23 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from repositories.user_plugin import UserPluginRepository
+from repositories.plugin import PluginRepository
 from typing import List, Dict
+from schemas.plugin import PluginListResponse
 
 router = APIRouter()
 
 # Route to list plugins associated with a user
-@router.get("/{user_id}/plugins/", response_model=List[Dict[str, int]])
+@router.get("/{user_id}/plugins/", response_model=PluginListResponse)
 def get_user_plugins(user_id: int, db: Session = Depends(get_db)):
     association_db = UserPluginRepository(db).get_list_dict()
-    user_plugins = [{"plugin_id": association["plugin_id"]} for association in association_db if association["user_id"] == user_id]
-    return user_plugins
+    user_plugins = []
+    plugin_repository = PluginRepository(db)
+    for association in association_db:
+        if association["user_id"] == user_id:
+            tempPlugin = plugin_repository.get_plugin_by_id(association["plugin_id"])
+            user_plugins.append(tempPlugin)
+    return {"plugins": user_plugins}
 
 
 # Route to associate a plugin with a user
