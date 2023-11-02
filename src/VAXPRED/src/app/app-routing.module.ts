1881c3e4d2696cf4f03a26f66de7d647e246b7f3
Fernando-A-Rocha <frocha2001@gmail.com>
2023-11-02 19:47:27 +0000

    Move store component to route /store

--- src/VAXPRED/src/app/app-routing.module.ts
+++ src/VAXPRED/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'profile', component: UserProfileComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'store', component: HomeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
