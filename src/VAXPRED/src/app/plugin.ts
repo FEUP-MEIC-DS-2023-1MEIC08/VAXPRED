c8ca2665267bef01a79bb80f54c5f5a286d9d85c
luis <joseluiscunharodrigues@gmail.com>
2023-11-07 16:10:44 +0000

    Added dependencies to plugin page

--- src/VAXPRED/src/app/plugin.ts
+++ src/VAXPRED/src/app/plugin.ts
@@ -2,7 +2,8 @@ export class Plugin {
     id: number;
     title: string;
     description: string;
-    // Add other properties as needed
+	// TODO: connect to db
+	dependencies: any[] = [{name: 'python', version:'3.9'}, {name: 'docker', version:'24.0'}];
   
     constructor(id: number, title: string, description: string) {
       this.id = id;
