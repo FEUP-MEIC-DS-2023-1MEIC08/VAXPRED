d89589685bef2a3c9cf901788e8e958cbe9958bc
Pedro Moreira <pedro2001moreira@gmail.com>
2023-11-09 11:52:23 +0000

    fix: clean unnecessary import

--- src/VAXPRED/src/app/user-profile/user-profile.component.ts
+++ src/VAXPRED/src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GridModule, CardModule, ButtonModule, AvatarModule, PaginationModule  } from '@coreui/angular';
-import { HttpClient } from '@angular/common/http';
 
 import { UserProfileService } from '../user-profile.service';
 
