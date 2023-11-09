1b255a99a1b8d0b6753383a4240fec12b2dfbbe1
Pedro Correia <up202006199@up.pt>
2023-11-09 16:07:02 +0000

    Revert "Fixed tests"
    This reverts commit 235f090143e0280c3fdbc0eef5445e9db833dbf4.

--- src/VAXPRED/src/app/search-page/plugin-card/plugin-card.component.spec.ts
+++ src/VAXPRED/src/app/search-page/plugin-card/plugin-card.component.spec.ts
@@ -2,16 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PluginCardComponent } from './plugin-card.component';
 
-import { MatDialogModule } from '@angular/material/dialog';
-
 describe('PluginCardComponent', () => {
   let component: PluginCardComponent;
   let fixture: ComponentFixture<PluginCardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PluginCardComponent],
-      imports:[MatDialogModule],
+      declarations: [PluginCardComponent]
     });
     fixture = TestBed.createComponent(PluginCardComponent);
     component = fixture.componentInstance;
