bfac998f2e745134c97a6b85521add4403f3d976
Leandro <jose.leandro2002@gmail.com>
2023-11-13 04:06:04 +0000

    added faqs table (missing association to plugins)

--- src/VAXPRED/api/main.py
+++ src/VAXPRED/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import plugins, user_plugins, users, tags, plugin_tag
+from routers import plugins, user_plugins, users, tags, plugin_tag, faqs
 from database import Base,engine
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -23,4 +23,5 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(plugins.router, prefix="/plugins", tags=["plugins"])
 app.include_router(tags.router, prefix="/tags", tags=["tags"])
 app.include_router(user_plugins.router, prefix="/users", tags=["user_plugins"])
-app.include_router(plugin_tag.router, prefix="/tags", tags=["tag_plugins"])
+app.include_router(faqs.router, prefix="/faqs", tags=["faqs"])
+# app.include_router(plugin_tag.router, prefix="/tags", tags=["tag_plugins"])
--- src/VAXPRED/api/models.py
+++ src/VAXPRED/api/models.py
@@ -20,6 +20,13 @@ plugin_tag_association = Table(
     UniqueConstraint('plugin_id', 'tag_id', name='unique_plugin_tag')
 )
 
+plugin_faq_association = Table(
+    'plugin_faq_association',
+    Base.metadata,
+    Column('plugin_id', Integer, ForeignKey('plugins.id')),
+    Column('faq_id', Integer, ForeignKey('faqs.id')),
+    UniqueConstraint('plugin_id', 'faq_id', name='unique_plugin_faq')
+)
 
 class Plugin(Base):
     __tablename__ = 'plugins'
@@ -37,6 +44,7 @@ class Plugin(Base):
 
     users = relationship("User", secondary=user_plugin_association, back_populates="plugins")
     tags = relationship("Tag", secondary=plugin_tag_association, back_populates="plugins")
+    faqs = relationship("FAQ", secondary=plugin_faq_association, back_populates="plugins")
 
 update_search_text_trigger = DDL('''
 CREATE TRIGGER set_search_text_trigger 
@@ -67,3 +75,10 @@ class Tag(Base):
 
     plugins = relationship("Plugin", secondary=plugin_tag_association, back_populates="tags")
 
+class FAQ(Base):
+    __tablename__ = 'faqs'
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String,  nullable=False, unique=False, index=True)
+    answer = Column(String,  nullable=False, unique=False, index=True)
+
+    plugins = relationship("Plugin", secondary=plugin_faq_association, back_populates="faqs")
