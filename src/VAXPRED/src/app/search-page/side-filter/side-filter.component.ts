218f8b6046c2c69eb59461837c66ad397c99fcc5
Nuno Jesus <nunomiguel533@gmail.com>
2023-11-15 15:48:31 +0000

    Fix: the plugin cards won't disappear anymore after reseting filters.

--- src/VAXPRED/src/app/search-page/side-filter/side-filter.component.ts
+++ src/VAXPRED/src/app/search-page/side-filter/side-filter.component.ts
@@ -4,90 +4,93 @@ import { Plugin } from 'src/app/plugin';
 import { ToolService } from 'src/app/plugin.service';
 
 @Component({
-  selector: 'app-side-filter',
-  templateUrl: './side-filter.component.html',
-  styleUrls: ['./side-filter.component.css']
+	selector: 'app-side-filter',
+	templateUrl: './side-filter.component.html',
+	styleUrls: ['./side-filter.component.css']
 })
 export class SideFilterComponent {
-  items: Plugin[] = [];
-  originalItems: Plugin[] = [];
-  sortingOption: string = 'original';
-  isRadioSelected: boolean = false;
-  toolTypes: string[]=[];
-  selectedToolTypes: { [key: string]: boolean } = {};
-  tags: string[] = [];
-  selectedTags: { [key: string]: boolean } = {};
+	items: Plugin[] = [];
+	originalItems: Plugin[] = [];
+	sortingOption: string = 'original';
+	isRadioSelected: boolean = false;
+	toolTypes: string[] = [];
+	selectedToolTypes: { [key: string]: boolean } = {};
+	tags: string[] = [];
+	selectedTags: { [key: string]: boolean } = {};
 
-  constructor(private toolService: ToolService) {
-    // this.items = this.toolService.getTools().slice();
+	constructor(private toolService: ToolService)
+	{
+		this.toolService.getPlugins().subscribe((data: any) => {
+			console.log(data);
 
-	this.toolService.getPlugins().subscribe((data: any) => {
-		console.log(data);
+			data.plugins.forEach((plugin: any) => {
+				this.items.push(
+					new Plugin(
+						plugin.id,
+						plugin.name,
+						plugin.description,
+						'assets/img/ydata.png', // plugin.image,
 
-		for (const plugin of data.plugins)
-		{
-			this.items.push(
-				new Plugin(
-					plugin.id,
-					plugin.name,
-					plugin.description,
-					'assets/img/ydata.png', // plugin.image,
-					'Default Plugin Type',  // plugin.type,
-					plugin.tags
-				));
+						'Data Quality',  // plugin.type,
+						plugin.tags
+					));
+			});
+
+			for (const plugin of data.plugins) {
+				
+			}
+			this.originalItems = this.items.slice();
+		});
+
+		this.toolTypes = this.toolService.getToolTypes();
+		this.tags = this.toolService.getTags();
+	}
+
+	/**
+	 * Resets the list to the initial format
+	 */
+	resetListToInitialFormat() {
+		this.items = this.originalItems.slice();
+		this.sortingOption = 'original';
+		this.isRadioSelected = false;
+		this.filterList();
+
+	}
+
+	/**
+	 * Sorts the list based on the selected option, either A-Z or Z-A
+	 */
+	toggleSorting(): void {
+		this.isRadioSelected = true;
+		if (this.sortingOption === 'aToZ') {
+			this.items.sort((a, b) => a.name.localeCompare(b.name));
+		} else if (this.sortingOption === 'zToA') {
+			this.items.sort((a, b) => b.name.localeCompare(a.name));
+		} else {
+			this.resetListToInitialFormat();
 		}
-  	});
+	}
 
-    this.toolTypes=this.toolService.getToolTypes();
-    this.originalItems = this.items.slice();
-    this.tags = this.toolService.getTags();
-  }
+	checkTag(item: Plugin): boolean {
+		for (const tag of item.tags) {
+			if (this.selectedTags[tag]) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  /**
-   * Resets the list to the initial format
-   */
-  resetListToInitialFormat() {
-    this.items = this.originalItems.slice();
-    this.sortingOption = 'original';
-    this.isRadioSelected = false;
-    this.filterList();
-   
-  }
+	/**
+	 * Filters the list based on the selected tool type and the tag
+	 */
+	filterList(): void {
+		const selectedTypes = Object.keys(this.selectedToolTypes).filter((type) => this.selectedToolTypes[type]);
+		const selectedTags = Object.keys(this.selectedTags).filter((tag) => this.selectedTags[tag]);
 
-  /**
-   * Sorts the list based on the selected option, either A-Z or Z-A
-   */
-  toggleSorting(): void {
-    this.isRadioSelected = true;
-    if (this.sortingOption === 'aToZ') {
-      this.items.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (this.sortingOption === 'zToA') {
-      this.items.sort((a, b) => b.name.localeCompare(a.name));
-    } else {
-      this.resetListToInitialFormat();
-    }
-  }
-
-  checkTag(item: Plugin) : boolean{
-    for(const tag of item.tags){
-      if(this.selectedTags[tag]){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
-   * Filters the list based on the selected tool type and the tag
-   */
-  filterList(): void {
-    const selectedTypes = Object.keys(this.selectedToolTypes).filter((type) => this.selectedToolTypes[type]);
-    const selectedTags = Object.keys(this.selectedTags).filter((tag) => this.selectedTags[tag]);
-
-    this.items = this.originalItems.filter((item: Plugin) => {
-      const typeMatch = selectedTypes.length === 0 || selectedTypes.includes(item.type);
-      const tagMatch = selectedTags.length === 0 || item.tags.some((tag) => selectedTags.includes(tag));
-      return typeMatch && tagMatch;
-    });
-  }
+		this.items = this.originalItems.filter((item: Plugin) => {
+			const typeMatch = selectedTypes.length === 0 || selectedTypes.includes(item.type);
+			const tagMatch = selectedTags.length === 0 || item.tags.some((tag) => selectedTags.includes(tag));
+			return typeMatch && tagMatch;
+		});
+	}
 }
