9f34e4cd1f7fb18d914146059de350c18fa0702e
Juan Ignacio Medone Sabatini <juanimedone@gmail.com>
2023-12-13 21:21:46 +0000

    all admin page tests being executed succesfully

--- src/VAXPRED/src/app/admin-page-plugin-form/admin-page-plugin-form.component.spec.ts
+++ src/VAXPRED/src/app/admin-page-plugin-form/admin-page-plugin-form.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminPagePluginFormComponent } from './admin-page-plugin-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('AdminPagePluginFormComponent', () => {
   let component: AdminPagePluginFormComponent;
@@ -8,7 +16,27 @@ describe('AdminPagePluginFormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AdminPagePluginFormComponent]
+      declarations: [AdminPagePluginFormComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatChipsModule,
+        MatIconModule,
+        FormsModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {} 
+        }
+      ]
     });
     fixture = TestBed.createComponent(AdminPagePluginFormComponent);
     component = fixture.componentInstance;
