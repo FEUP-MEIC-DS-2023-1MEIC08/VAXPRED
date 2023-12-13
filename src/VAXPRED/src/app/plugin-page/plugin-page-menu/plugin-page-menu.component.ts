fd99cee5eb475a8075e00934b97c962af42afc55
beatrizfontao <anabfontao@gmail.com>
2023-12-13 21:11:22 +0000

    commented plugin-page and plugin-page-menu

--- src/VAXPRED/src/app/plugin-page/plugin-page-menu/plugin-page-menu.component.ts
+++ src/VAXPRED/src/app/plugin-page/plugin-page-menu/plugin-page-menu.component.ts
@@ -6,12 +6,33 @@ import { Plugin } from '../../plugin';
   templateUrl: './plugin-page-menu.component.html',
   styleUrls: ['./plugin-page-menu.component.css']
 })
+
+/**
+ * Class: PluginPageMenuComponent
+ * Purpose: Angular component responsible for displaying a menu of a plugin page
+ */
 export class PluginPageMenuComponent {
+
+  /**
+   * Input: selectedTab
+   * Purpose: Holds the index of the selected tab (default value is 0)
+   */
   @Input() selectedTab: number = 0;
+
+  /**
+   * Input: plugin
+   * Purpose: Holds data related to the plugin being displayed on the page. Default value is an instance of Plugin with default values
+   */
   @Input() plugin: Plugin = new Plugin(0, '', '', '', '', '', new Date(), new Date(), '','', [], 2, []);
 
   getFormattedFAQ(): string {
     const faqs = this.plugin.faq;
+    /**
+     * Method: formattedFAQs
+     * Returns: HTML-formatted string containing questions in bold and their corresponding answers separated by line breaks, using the 'plugin' property of the class
+     * Purpose: Format the FAQs from the plugin object into an HTML string
+     * Parameters: None
+    */
     const formattedFAQs = faqs.map((faq: any) => {
       const [question, answer] = [faq.question, faq.answer];
       if (question) {
--- src/VAXPRED/src/app/plugin-page/plugin-page.component.ts
+++ src/VAXPRED/src/app/plugin-page/plugin-page.component.ts
@@ -8,9 +8,31 @@ import { ToolService } from '../plugin.service';
   templateUrl: './plugin-page.component.html',
   styleUrls: ['./plugin-page.component.css']
 })
+
+/**
+ * Class: PluginPageComponent
+ * Purpose: Angular component responsible for displaying a plugin page within a dialog
+ */
 export class PluginPageComponent {
+
+  /**
+   * Property: selectedTab
+   * Purpose: Holds the index of the selected tab (default value is 0)
+   */
   selectedTab: number = 0;
+
+  /**
+   * Property: plugin
+   * Purpose: Holds data related to the plugin being displayed on the page. Default value is an instance of Plugin with default values
+   */
   plugin: Plugin = new Plugin(0, '', '', '', '', '', new Date(), new Date(), '','', [], 2, []);
+
+  /**
+   * Constructor: PluginPageComponent
+   * Parameter dialogRef: Reference to the dialog instance to control the opening/closing of the dialog
+   * Parameter data Data: injected into the dialog, containing references and plugin data
+   * Parameter toolService: Service handling plugin-related tools and functionalities
+ */
   constructor(
     public dialogRef: MatDialogRef<PluginPageComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { dialogRef: MatDialogRef<PluginPageComponent, any>; plugin: Plugin },
@@ -19,6 +41,10 @@ export class PluginPageComponent {
     this.plugin = data.plugin;
   }
 
+  /**
+   * Method: closeDialog()
+   * Purpose: Close the dialog when called
+   */
   closeDialog() {
     this.data.dialogRef.close();
   }
